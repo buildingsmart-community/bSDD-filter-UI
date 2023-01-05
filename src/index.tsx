879f2c37c17f796d579254c4fdb576f24546cdad
Jan Brouwer <jan@brewsky.nl>
2023-01-05 17:36:22 +0100

    work-in-progress

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import BsddSearch from './components/BsddSearch'
-import reportWebVitals from './reportWebVitals'
 
-function callback(data: any) {
-  console.log(data)
-  alert(JSON.stringify(data, null, 2))
+// function callback(data: any) {
+//   console.log(data)
+//   alert(JSON.stringify(data, null, 2))
+// }
+
+// const config = {
+//   defaultDomains: [
+//     {
+//       value: 'https://identifier.buildingsmart.org/uri/digibase/volkerwesselsbv-0.1',
+//       label: 'VolkerWessels Bouw & Vastgoed',
+//     },
+//   ],
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+function insertBsddSearch(domElement: HTMLElement, callback: any, config: any) {
+  const root = ReactDOM.createRoot(domElement as HTMLElement)
+  root.render(
+    <React.StrictMode>
+      <BsddSearch callback={callback} config={config} />
+    </React.StrictMode>,
+  )
 }
-const config = {
-  defaultDomains: [
-    {
-      value: 'https://identifier.buildingsmart.org/uri/digibase/volkerwesselsbv-0.1',
-      label: 'VolkerWessels Bouw & Vastgoed',
-    },
-  ],
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-
-root.render(
-  <React.StrictMode>
-    <BsddSearch callback={callback} config={config} />
-  </React.StrictMode>,
-)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+// reportWebVitals()
 
-export { BsddSearch }
+export { BsddSearch, insertBsddSearch }
