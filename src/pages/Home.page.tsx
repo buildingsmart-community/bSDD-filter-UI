100f7a46aa486b186071540efcc844794ea97f06
Amar <amar.ramdas@asrr.nl>
2023-11-07 14:26:02 +0100

    Create working base version

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,8 +1,8 @@
 import {ColorSchemeToggle} from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import {Button, Card, Collapse, Container, Group, Indicator, Stack} from "@mantine/core";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {IconCheck, IconStereoGlasses} from "@tabler/icons-react";
-import CategoryCollapse from "../components/CategoryCollapse";
+// import CategoryCollapse from "../components/CategoryCollapse";
 
 type IfcSlabTypeType = 'IfcSlabType';
 type IfcClassificationReferenceType = 'IfcClassificationReference';
@@ -168,9 +168,30 @@ function groupBy(array, property) {
 }
 
 export function BsddCard(props: { item: IfcSlabType, class: any }) {
-    let color = "orange"
+    let color = "blue"
 
     if (!props.class) color = "red"
+
+    function determineColor(item: IfcSlabType, found: any) {
+        const ifcClassificatonReference =  item.hasAssociations?.filter(it => it.type === "IfcClassificationReference")
+
+        if (!ifcClassificatonReference) return "orange"
+
+        for(let classRelation in found.classRelations) {
+            // @ts-ignore
+            console.log(classRelation.relatedClassUri, ifcClassificatonReference.location, classRelation.relatedClassUri === ifcClassificatonReference.location)
+            // @ts-ignore
+            if (classRelation.relatedClassUri === ifcClassificatonReference.location) return "green"
+        }
+
+        return "red"
+
+
+        // if (found.)
+
+    }
+
+    if (props.class) color = determineColor(props.item, props.class)
 
     return <Card key={props.item.name} my={"xs"}>
         <Group>
@@ -183,56 +204,154 @@ export function BsddCard(props: { item: IfcSlabType, class: any }) {
                 {props.item.name}
             </div>
 
-            <pre>
-            {JSON.stringify(props.class, null, 2)}
-            </pre>
 
-            <pre>
-            {JSON.stringify(props.item.hasAssociations, null, 2)}
-            </pre>
+            {/*<pre>*/}
+            {/*{JSON.stringify(props.class, null, 2)}*/}
+            {/*</pre>*/}
+
+            {/*<pre>*/}
+            {/*{JSON.stringify(props.item, null, 2)}*/}
+            {/*</pre>*/}
         </Group>
     </Card>
 }
 
-function CategoryCollap2se(props: { category: string, opened: any, data: BimBasisObjectsResponse, items: any[] }) {
+// function CategoryCollap2se(props: { category: string, opened: any, data: BimBasisObjectsResponse, items: any[] }) {
+//
+//     function determineBsddClass(item: IfcSlabType) {
+//         // if none of the descriptions in data is the same as item.description, then color is red
+//         // if description is present, orange
+//
+//         // if ifc type and predefined type are in the found data object, material is the same, nslsfb is the same
+//         let found;
+//
+//         props.data?.classes.filter((dataItem: any) => {
+//             if (dataItem.referenceCode === item.description) {
+//                 found = dataItem
+//             }
+//         })
+//
+//
+//         if (!found) return false
+//
+//         // let result = fetch(found.referenceCode.location)
+//         //  console.log(result)
+//
+//         console.log(found, item)
+//
+//         return found
+//     }
+//
+//     // fetch category uri
+//
+//     const {opened, category} = props;
+//
+//     return <Collapse in={!opened[category]}>
+//         {/* Iterate over items for each category */}
+//         {props.items.map((item) => {
+//             return (
+//                 <BsddCard item={item} class={determineBsddClass(item)} key={item.id}/>
+//             );
+//         })}
+//     </Collapse>
+// }
 
-    function determineBsddClass(item: IfcSlabType) {
+interface CategoryCollapseProps {
+    category: string;
+    opened: Record<string, boolean>;
+    bbbr: BimBasisObjectsResponse;
+    items: IfcSlabType[];
+}
+
+function CategoryCollapse(props: CategoryCollapseProps) {
+    const {category, opened, items} = props;
+    const [isLoading, setIsLoading] = useState(true);
+    const [data, setData] = useState<any>();
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+        // Function to fetch data
+        const fetchData = async () => {
+            try {
+                // Start by setting loading to true
+                setIsLoading(true);
+
+                const found = determineBsddClass(category, props.bbbr)
+
+                if (found === false) return false
+                // console.log(found)
+                const encodedUri = encodeURIComponent(found.uri);
+                const targetUrl = `https://bs-dd-api-prototype.azurewebsites.net/api/Class/v1?uri=${encodedUri}&includeClassProperties=true&includeChildClassReferences=true&includeClassRelations=true`;
+
+                // Fetch data from the API
+                const response = await fetch(targetUrl);
+
+
+                // If the response is not ok, throw an error
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+
+                // Parse the JSON data
+                const result = await response.json();
+
+                // console.log(result)
+
+                // Update state with the fetched data
+                setData(result);
+            } catch (e) {
+                // If an error occurred, set the error state
+                setError(e.message);
+            } finally {
+                // Set loading to false when loading is complete
+                setIsLoading(false);
+            }
+        };
+
+        // Call the fetchData function
+        fetchData();
+    }, []); // The empty array ensures this effect runs only once after initial render
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    // If there was an error, show an error message
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+    function determineBsddClass(category: string, bbbr: BimBasisObjectsResponse) {
         // if none of the descriptions in data is the same as item.description, then color is red
         // if description is present, orange
 
         // if ifc type and predefined type are in the found data object, material is the same, nslsfb is the same
         let found;
 
-        props.data?.classes.filter((dataItem: any) => {
-            if (dataItem.referenceCode === item.description) {
+        bbbr.classes.filter((dataItem: any) => {
+            if (dataItem.code === category) {
                 found = dataItem
             }
         })
 
-
         if (!found) return false
-
-        // let result = fetch(found.referenceCode.location)
-        //  console.log(result)
-
-        console.log(found, item)
 
         return found
     }
 
-    // fetch category uri
 
-    const {opened, category} = props;
-
-    return <Collapse in={!opened[category]}>
-        {/* Iterate over items for each category */}
-        {props.items.map((item) => {
-            return (
-                <BsddCard item={item} class={determineBsddClass(item)} key={item.id}/>
-            );
-        })}
-    </Collapse>
+    return (
+        <Collapse in={!opened[category]}>
+            {items.map((item, index) => {
+                return (
+                    <BsddCard item={item} class={data} key={index}/>
+                );
+            })}
+        </Collapse>
+    );
 }
+
 
 export function HomePage() {
     // State for the fetched data
@@ -312,16 +431,16 @@ export function HomePage() {
 
                             {/* Collapse component that shows/hides the items */}
 
-                            {/* @ts-ignore */}
-                            <CategoryCollapse category={category} items={items} opened={opened} data={data}/>
+                            {/*@ts-ignore*/}
+                            <CategoryCollapse category={category} items={items} opened={opened} bbbr={data}/>
                         </div>
                     ))}
                 </Stack>
             </Container>
 
-            <pre>
-                {JSON.stringify(data, null, 2)}
-            </pre>
+            {/*<pre>*/}
+            {/*    {JSON.stringify(data, null, 2)}*/}
+            {/*</pre>*/}
         </>
     );
 }
