b537900ebc0716d92a6498272828b7fd2a2485d1
janbrouwer <jan@brewsky.nl>
2025-01-31 10:52:29 +0000

    Deploying to gh-pages from @ buildingsmart-community/bSDD-filter-UI@ecb3b14d05bbcdf252c1ef70dff9ad91b2694b62 🚀

---
+++ main/assets/bsdd_search-c05a7287.js
@@ -0,0 +1,1 @@
+import{j as r,A as o,c as n,a as s}from"./defaultSettings-9a56c514.js";import{B as t}from"./BsddSearch-0043ae9f.js";import{u as a}from"./useCefSharpBridge-726dc972.js";function i(){const e=a();return r.jsx(o,{value:e,children:r.jsx(t,{})})}function c(){return r.jsx(s,{children:r.jsx(i,{})})}n.createRoot(document.getElementById("root")).render(r.jsx(c,{}));
