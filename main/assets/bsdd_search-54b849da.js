275c3a0d242857b97442e5d15c2ef508956f3788
jbrouwerdigibase <143397320+jbrouwerdigibase@users.noreply.github.com>
2025-02-25 18:53:49 +0000

    Deploying to gh-pages from @ buildingsmart-community/bSDD-filter-UI@5b5c09fdff80466e630fdbe4d06174fc1090c042 🚀

--- main/assets/bsdd_search-54b849da.js
+++
@@ -1,1 +0,0 @@
-import{j as r,A as o,c as n,a as s}from"./defaultSettings-1df5a3d3.js";import{B as t}from"./BsddSearch-d8aa14fd.js";import{u as a}from"./useCefSharpBridge-d707f018.js";function i(){const e=a();return r.jsx(o,{value:e,children:r.jsx(t,{})})}function c(){return r.jsx(s,{children:r.jsx(i,{})})}n.createRoot(document.getElementById("root")).render(r.jsx(c,{}));
