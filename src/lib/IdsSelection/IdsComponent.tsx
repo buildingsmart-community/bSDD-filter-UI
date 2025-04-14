52fcbdd6dc5a12792a92ccc8f79a72e1191aa16e
Rene Aparicio Saez <rene.aparicio.saez@waternet.nl>
2025-04-14 13:46:56 +0200

    formatting

--- src/lib/IdsSelection/IdsComponent.tsx
+++ src/lib/IdsSelection/IdsComponent.tsx
@@ -3,9 +3,7 @@ import { ThunkDispatch, UnknownAction } from '@reduxjs/toolkit';
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useAppSelector, useAppDispatch } from '../common/app/hooks';
-import {
-  selectMainDictionary,
-} from '../common/slices/settingsSlice';
+import { selectMainDictionary } from '../common/slices/settingsSlice';
 import {
   fetchDictionaryClasses,
   selectDictionaryClasses,
@@ -15,7 +13,7 @@ import { ClassListItemContractV1Classes } from '../common/BsddApi/BsddApiBase';
 import Importer from './features/Importer/Importer';
 import Validator from './features/Validator/Validator';
 import type { RootState } from '../common/app/store';
-import type { IdsClass } from "./types/types";
+import type { IdsClass } from './types/types';
 
 const fetchBsddClasses = async (
   ids: IdsClass,
@@ -26,19 +24,18 @@ const fetchBsddClasses = async (
   const classes = selectDictionaryClasses(state, location);
   if (classes) return;
 
-  await dispatch(fetchDictionaryClasses(location))
-  .then(async result =>{
+  await dispatch(fetchDictionaryClasses(location)).then(async (result) => {
     if (result.payload) {
       var bsdd = result.payload as ClassListItemContractV1Classes[];
-      
-      const barSet = new Set(ids.ids.specifications.specification.map(specification => specification._name));
-      const matches = bsdd.filter(obj => obj.name && barSet.has(obj.name));
-      
-      var uris = Array.from(matches.values()).map(match => match.uri as string);
+
+      const barSet = new Set(ids.ids.specifications.specification.map((specification) => specification._name));
+      const matches = bsdd.filter((obj) => obj.name && barSet.has(obj.name));
+
+      var uris = Array.from(matches.values()).map((match) => match.uri as string);
       await dispatch(updateFilterDictionaryClassificationUris(uris));
       return;
     }
-  })
+  });
 
   return;
 };
@@ -50,29 +47,27 @@ function IdsComponent() {
   const mainDictionary = useAppSelector(selectMainDictionary);
   const dispatch = useAppDispatch();
   useEffect(() => {
-    if(!mainDictionary || !mainDictionary.ifcClassification.location || !ids){
+    if (!mainDictionary || !mainDictionary.ifcClassification.location || !ids) {
       return;
     }
 
     const fetchBsddData = async () => {
-      fetchBsddClasses(ids, mainDictionary.ifcClassification.location, state, dispatch)
+      fetchBsddClasses(ids, mainDictionary.ifcClassification.location, state, dispatch);
     };
 
     fetchBsddData();
   }, [ids, mainDictionary, state, dispatch]);
 
   return (
-    <div>
-    <Importer/>
-      {ids && mainDictionary && (
-        <Validator />
-      )}
+    <>
+      <Importer />
+      {ids && mainDictionary && <Validator />}
       {!mainDictionary && (
         <Alert mx="md" mt="xl">
           {t('idsComponent.noBsddDomainSelected')}
         </Alert>
       )}
-    </div>
+    </>
   );
 }
 
